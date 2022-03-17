4494882cdb5c9a663b6a680a885ec2d2cbe145cd
Júlio César <juliojc.jord@gmail.com>
2022-03-17 16:55:13 -0300

    Emitindo eventos

--- book/src/app/components/primary-button/primary-button.component.ts
+++ book/src/app/components/primary-button/primary-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-primary-button',
@@ -12,10 +12,15 @@ export class PrimaryButtonComponent implements OnInit {
   @Input()
   color!:string;
   
+  @Output()
+  onViewDetails = new EventEmitter<any>();
 
   constructor() { }
-
+  
   ngOnInit(): void {
   }
 
+  clickViewDetails(index:number){
+    this.onViewDetails.emit({id: index})
+  }
 }
--- book/src/stories/Button.stories.ts
+++ book/src/stories/Button.stories.ts
@@ -2,7 +2,7 @@
 import { moduleMetadata } from '@storybook/angular';
 import { Story, Meta } from '@storybook/angular/types-6-0';
 import { PrimaryButtonComponent } from 'src/app/components/primary-button/primary-button.component';
-
+import { action } from '@storybook/addon-actions'
 // More on default export: https://storybook.js.org/docs/angular/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
@@ -18,20 +18,21 @@ export default {
 
 // More on component templates: https://storybook.js.org/docs/angular/writing-stories/introduction#using-args
 const Template: Story<PrimaryButtonComponent> = (args: PrimaryButtonComponent) => ({
-  props: args,
-  template:`<app-primary-button [label]="label" [color]="'#ffff'"></app-primary-button>` 
+  props: {
+    ...args,
+    onViewDetails:{ onViewDetails: action('onViewDetails')}
+  },
+  template:`<app-primary-button [label]="label" [color]="'#ffff'" (onViewDetails)="onViewDetails($event)"></app-primary-button>` 
 });
-
 
 export const Default = Template.bind({});
 Default.args = {
+  color:'#fff',
   label: 'Default Button',
 };
-
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/angular/writing-stories/args
 Primary.args = {
-  
   label: 'Button',
 };
