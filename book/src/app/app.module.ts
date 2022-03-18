655eb23a1893e56c204f218903b1333dc73a9305
Júlio César <juliojc.jord@gmail.com>
2022-03-18 17:21:43 -0300

    Adição de componente real

--- book/src/app/app.module.ts
+++ book/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SwiperModule } from 'swiper/angular';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PrimaryButtonComponent } from './components/primary-button/primary-button.component';
+import { SwiperComponent } from './components/swiper/swiper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PrimaryButtonComponent
+    PrimaryButtonComponent,
+    SwiperComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
