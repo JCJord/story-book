903ab7adaa3cf9db78fda4b078f2b460b9ef9bfb
Júlio César <juliojc.jord@gmail.com>
2022-03-21 17:11:35 -0300

    Adição stories slider e alteração home page

--- book/src/stories/swiper.stories.ts
+++ book/src/stories/swiper.stories.ts
@@ -1,35 +1,37 @@
 // also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
-import { HttpClient, HttpHandler } from '@angular/common/http';
 import { moduleMetadata } from '@storybook/angular';
 import { Story, Meta } from '@storybook/angular/types-6-0';
 
-import { HomeComponent } from 'src/app/pages/home/home.component';
+import { action } from '@storybook/addon-actions'
+import { SwiperComponent } from 'src/app/components/swiper/swiper.component';
 import { DogServiceService } from 'src/app/services/dog-service.service';
 // More on default export: https://storybook.js.org/docs/angular/writing-stories/introduction#default-export
 export default {
-  title: 'Pages/Pages',
-  component: HomeComponent,
+  title: 'Components/Swiper',
+  component: SwiperComponent,
   // More on argTypes: https://storybook.js.org/docs/angular/api/argtypes
   argTypes: { },
   decorators:[
     moduleMetadata({
-      declarations:[HomeComponent],
-      providers:[DogServiceService,HttpClient,HttpHandler]
+      imports:[],
+      declarations:[SwiperComponent],
     })
   ]
 } as Meta;
 
 // More on component templates: https://storybook.js.org/docs/angular/writing-stories/introduction#using-args
-const Template: Story<HomeComponent> = (args: HomeComponent) => ({
+const Template: Story<SwiperComponent> = (args: SwiperComponent) => ({
   props: {
     ...args,
-    
+   
   },
-  template:`<app-home></app-home>` 
+  template:`<app-swiper [breeds]="breeds"></app-swiper>` 
 });
 
-export const HomePage = Template.bind({});
-HomePage.args = {
-
-
+export const Slider = Template.bind({});
+Slider.args = {
+    breeds:[
+    'https://cdn.britannica.com/60/8160-050-08CCEABC/German-shepherd.jpg',
+    'https://cdn.britannica.com/60/8160-050-08CCEABC/German-shepherd.jpg',
+    'https://cdn.britannica.com/60/8160-050-08CCEABC/German-shepherd.jpg'],
 };
