d4b32b2aea07fd9d7eecbae42bdc40498b1e8b13
Júlio César <juliojc.jord@gmail.com>
2022-03-23 10:38:59 -0300

    modulo de animação

--- book/src/app/components/swiper/swiper.component.ts
+++ book/src/app/components/swiper/swiper.component.ts
@@ -1,21 +1,40 @@
+import { state, style, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-swiper',
   templateUrl: './swiper.component.html',
-  styleUrls: ['./swiper.component.scss']
+  styleUrls: ['./swiper.component.scss'],
+  animations: [
+    trigger('divState',[
+      state('normal',style({
+        backgroundColor:'red',
+        maxHeight:'400px'
+      })),
+      state('highlighted',style({
+        backgroundColor:'blue',
+        maxHeight:'0px'
+      }))
+    ])
+  ]
 })
 export class SwiperComponent {
 
   @Input()
   breeds:any = []
 
+  state = 'normal'
+
   swiperConfig: any = {
     slidesPerView: 2,
     spaceBetween: 10,
     initialSlide : 1,
-}
+  }
 
 
   constructor() { }
 
+  changeState(){
+    this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal'
+    console.log(this.state)
+  }
 }
