086fa736d2d8c2c847b6bcdad75b548bf58cd74d
Júlio César <juliojc.jord@gmail.com>
2022-03-23 17:20:45 -0300

    Adição sidenav

--- book/src/app/components/header/header.component.ts
+++ book/src/app/components/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { state, style, trigger } from '@angular/animations';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations:[
+    trigger('sideNav',[
+      state('open',style({
+        width:'200px'
+      })),
+      state('closed',style({
+        width:'0px'
+      }))
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
+  @Output()
+  menuState = new EventEmitter<string>();
+
+  state = 'closed'
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeState(){
+    this.state === 'closed' ? this.state = 'open' : this.state = 'closed';
+    this.menuState.emit(this.state)
+  }
 }
