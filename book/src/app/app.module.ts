204795f34de695c7407274b7aa3cb62c40cadccf
Júlio César <juliojc.jord@gmail.com>
2022-03-23 10:38:03 -0300

    adição de header

--- book/src/app/app.module.ts
+++ book/src/app/app.module.ts
@@ -3,20 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { SwiperModule } from 'swiper/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PrimaryButtonComponent } from './components/primary-button/primary-button.component';
 import { SwiperComponent } from './components/swiper/swiper.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PrimaryButtonComponent,
-    SwiperComponent
+    SwiperComponent,
+    HeaderComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     SwiperModule,
