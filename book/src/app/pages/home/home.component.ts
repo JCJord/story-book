09f87dff4eb64e016e8966d8aae137505188ee4e
Júlio César <juliojc.jord@gmail.com>
2022-04-13 10:39:33 -0300

    usando operador pipe map

--- book/src/app/pages/home/home.component.ts
+++ book/src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { lastValueFrom, map } from 'rxjs';
 import { DogServiceService } from 'src/app/services/dog-service.service';
 
 @Component({
@@ -15,12 +15,29 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.test()
+    
+  
+
   }
   async test(){
     let data:any = await lastValueFrom(this.dogService.getData())
-    this.breeds = data.message
-  
+    this.breeds = data.message 
+    console.log(data)
   }
+
+  pipeMethod(){
+    const g = this.dogService.getData().pipe(map((e:any)=>{
+      
+
+      return {
+        message:['No label','no label'], status:'not running'
+      }
+    })).subscribe((e)=>{
+      console.log(e)
+    }) 
+   
+  }
+  
   getDetails(e:any){
     this.details === 0 ? this.details = e.id : this.details = e.id+this.details;
   }
